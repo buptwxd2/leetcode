6fd26bdf0a3953c8275a6d916333c53accc3ca5a
wanga26 <alex.wang@emc.com>
2020-01-22 17:58:05 +0800

    Add 217

---
+++ Round_1/217. Contains Duplicate/solution_2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        new_set = set()
+
+        for num in nums:
+            if num in new_set:
+                return True
+
+            new_set.add(num)
+
+        return False
+    
